11741a3e616131f94d9ce29cc88563951df0fc4a
Arthur Rosch <rosch.arthur@gmail.com>
2022-12-28 18:02:14 -0300

    Home Finalizada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ import {
   FormContainer,
   CountdownContainer,
   Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
 } from './style'
 
 export function Home() {
@@ -12,11 +15,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou Trabalhar em</label>
-          <input id="task" />
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">Durante</label>
-          <input type="number" id="minutesAmount" />
-
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>Minutos.</span>
         </FormContainer>
 
@@ -27,10 +45,10 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -31,6 +31,39 @@ export const FormContainer = styled.div`
   font-weight: bold;
   flex-wrap: wrap;
 `
+const BaseInput = styled.input`
+  height: 2.5rem;
+  padding: 0 0.5rem;
+
+  border: 0;
+  font-weight: bold;
+  font-size: 1.125rem;
+
+  background: transparent;
+  color: ${(props) => props.theme['gray-100']};
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const CountdownContainer = styled.div`
   font-size: 10rem;
   line-height: 8rem;
@@ -55,4 +88,31 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  color: ${(props) => props.theme['gray-100']};
+  background: ${(props) => props.theme['green-500']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled)hover {
+    background: ${(props) => props.theme['green-700']};
+  }
 `
